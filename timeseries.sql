5115cc584405c2c0e5e4c19034d8c0b61e3663a6
Jose Arthur Benetasso Villanova <jose.villanova@neon.com.br>
2024-11-23 16:47:01 -0300

    Feat(postgres): Insert Over 9000 reference

--- timeseries.sql
+++ timeseries.sql
@@ -8,12 +8,12 @@ CREATE TABLE metrics (
 
 INSERT INTO metrics (SELECT 'requests',
                             g.t,
-                            random(0, 100)
+                            random(3000, 6000)
                        FROM generate_series(CURRENT_TIMESTAMP - INTERVAL '1 day',
                                             CURRENT_TIMESTAMP + INTERVAL '10 minutes',
                                             INTERVAL '15 seconds') as g(t));
 
 UPDATE metrics
-   SET gauge = gauge + 100
+   SET gauge = gauge + 6500
  WHERE t > CURRENT_TIMESTAMP + '1 minute'
    AND t <= CURRENT_TIMESTAMP + '5 minutes';
